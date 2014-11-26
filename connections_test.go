485f6ae4513e377844630bd1c96826cf285eef4d
Pekka Pöyry <pekka.poyry@gmail.com>
2014-11-26 15:51:37 +0200

    Add some tests

---
+++ connections_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestCreateHttpServerConnection(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		assert.Equal(t, "REQUEST", r.FormValue("msg"))
+		fmt.Fprint(w, "RESPONSE")
+	}))
+	defer ts.Close()
+
+	send := make(chan []byte)
+	recv := make(chan []byte)
+
+	if assert.Equal(t, createServerConnection(ts.URL, &send, &recv), true) {
+		send <- []byte("REQUEST")
+		response := <-recv
+		assert.Equal(t, "RESPONSE", string(response))
+	}
+}
